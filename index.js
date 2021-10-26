513d615a29c9763af1d7f8000db4c507695d7346
Paolo Giua <paolo.giuaa@gmail.com>
2021-10-26 23:32:14 +0200

    Added download button

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.get('/cv.pdf', async (req, res) => {
+  const pdf = await getPDF(port, req.query);
+  res.send(pdf);
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + req.path));
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
+
+async function getPDF(port, options) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:' + port);
+
+  if(options.dark == 'true')
+    await page.click('#dark-button')
+  if(options.rounded == 'true')
+    await page.click('#rounds-button')
+
+  const pdf = await page.pdf({
+  	format: 'A4',
+    printBackground: true,
+    preferCSSPageSize: true
+  });
+
+  await browser.close();
+  return pdf;
+}
