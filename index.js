4549054006c35690ef24e7a2379af3837206ba49
Paolo Giua <paolo.giuaa@gmail.com>
2021-10-28 20:17:53 +0200

    Added download in png format

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ app.get('/cv.pdf', async (req, res) => {
   res.send(pdf);
 })
 
+app.get('/cv.png', async (req, res) => {
+  const png = await getPNG(port, req.query);
+  res.send(png);
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + req.path));
 })
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`);
 })
 
 async function getPDF(port, options) {
@@ -28,16 +33,45 @@ async function getPDF(port, options) {
   await page.goto('http://localhost:' + port);
 
   if(options.dark == 'true')
-    await page.click('#dark-button')
+    await page.click('#dark-button');
   if(options.rounded == 'true')
-    await page.click('#rounds-button')
+    await page.click('#rounds-button');
 
   const pdf = await page.pdf({
-  	format: 'A4',
+    format: 'A4',
     printBackground: true,
     preferCSSPageSize: true
   });
 
   await browser.close();
   return pdf;
+}
+
+async function getPNG(port, options) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:' + port);
+
+  if(options.dark == 'true')
+    await page.click('#dark-button');
+  if(options.rounded == 'true')
+    await page.click('#rounds-button');
+  await page.addStyleTag({content: `
+    * {
+      transition: none !important;
+    }
+
+    html {
+      background-color: transparent !important;
+    }
+  
+  `});
+
+  await page.emulateMediaType('print');
+  const png = await page.screenshot({ 
+    fullPage: true
+  });
+
+  await browser.close();
+  return png;
 }
